8bdbd3ae4c77d8257429a48045efb1a606754e42
nandhini929 <vnandhini658@gmail.com>
2026-05-22 11:29:47 +0530

    update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,55 @@
+import Header from '../components/Header'
+import StoriesRow from '../components/StoriesRow'
+import PostCard from '../components/PostCard'
+import BottomNav from '../components/BottomNav'
+import user1 from '../assets/user1.jpeg'
+import user2 from '../assets/user2.jpeg'
+import user3 from '../assets/user3.jpeg'
+
+const posts = [
+  {
+    id: 1,
+    username: "priya_art",
+    location: "Chennai, India",
+    avatar: user1,
+    postImage: user2,
+    likes: 1248,
+    caption: "Golden hour hits different in Chennai ✨",
+    time: "2 hours ago"
+  },
+  {
+    id: 2,
+    username: "dev_karan",
+    location: "Bangalore, India",
+    avatar: user2,
+    postImage: user3,
+    likes: 892,
+    caption: "Shipped my first React project today 🚀",
+    time: "5 hours ago"
+  },
+  {
+    id: 3,
+    username: "sara_clicks",
+    location: "Mumbai, India",
+    avatar: user3,
+    postImage: user1,
+    likes: 3421,
+    caption: "Life is beautiful 🌸",
+    time: "1 day ago"
+  },
+]
+
 function Home() {
   return (
-    <div>
-      <h1>Instagram Home</h1>
-
-      <div>
-        <p>Story Section</p>
+    <div className="app">
+      <Header />
+      <div className="feed">
+        <StoriesRow />
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
       </div>
-
-      <div>
-        <p>Posts Feed</p>
-      </div>
+      <BottomNav />
     </div>
   )
 }
